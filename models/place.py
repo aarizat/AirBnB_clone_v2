9c81b99abf80743ffda1b2d829c596d3c6f1cb55
aarizat <aarizatr@gmail.com>
2020-12-16 22:42:05 +0000

    Set parameter to ForeignKey

--- models/place.py
+++ models/place.py
@@ -13,11 +13,11 @@ if getenv("HBNB_TYPE_STORAGE") == "db":
     place_amenity = Table(
         "place_amenity", Base.metadata,
         Column("place_id", String(60), ForeignKey(
-            "places.id"), primary_key=True, nullable=False,
-            cascade="all, delete"),
+            "places.id", ondelete="CASCADE", onupdate="CASCADE"),
+            primary_key=True, nullable=False),
         Column("amenity_id", String(60), ForeignKey(
-            "amenities.id"), primary_key=True, nullable=False,
-            cascade="all, delete"))
+            "amenities.id", ondelete="CASCADE", onupdate="CASCADE"),
+            primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
