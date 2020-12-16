dd7f67081759b604fe00364ae66cfcd6b3f1a955
aarizat <aarizatr@gmail.com>
2020-12-16 22:14:53 +0000

    Add viewonly parameter

--- models/amenity.py
+++ models/amenity.py
@@ -12,4 +12,5 @@ class Amenity(BaseModel, Base):
     place_amenities = relationship(
         "Place",
         secondary="place_amenity",
-        back_populates="amenities")
+        back_populates="amenities",
+        viewonly=False)
