707f93aecaea9493d6954e056b72b5cf73cc75cb
Deyber2000 <garzondeiber6@gmail.com>
2020-12-15 22:15:12 -0500

    map state to database

--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        "City",
+        cascade="delete",
+        backref="state",
+        passive_deletes=True,
+        single_parent=True
+    )
+    if getenv('HBNB_TYPE_STORAGE') != "db":
+        @property
+        def cities(self):
+            """getter attribute cities"""
+            city_l = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    city_l.append(city)
+            return city_l
