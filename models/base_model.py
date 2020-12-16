611005a751bb1572be94bbd6a3214960b9d06435
Deyber2000 <garzondeiber6@gmail.com>
2020-12-15 22:12:34 -0500

    add delete method

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,14 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True)
-    #created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
-    #updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
         for key, value in kwargs.items():
             if key in {"created_at", "updated_at"}:
                 value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
@@ -33,7 +34,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         storage.new(self)
         storage.save()
 
@@ -44,7 +45,7 @@ class BaseModel:
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
-        #dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
         try:
             del dictionary['_sa_instance_state']
         except KeyError:
