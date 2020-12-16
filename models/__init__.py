96833182c2e7d3cec4936ab2e605adad2b3a96d1
aarizat <aarizatr@gmail.com>
2020-12-16 22:09:34 +0000

    Import all model classes

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,12 @@
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+from models.state import State
+from models.city import City
 
 
 TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
