f52364c4b38c1d21aa33ebb16cef6b254ea1b326
aarizat <aarizatr@gmail.com>
2020-12-16 22:33:56 +0000

    Add cascade paramater

--- models/place.py
+++ models/place.py
@@ -13,9 +13,11 @@ if getenv("HBNB_TYPE_STORAGE") == "db":
     place_amenity = Table(
         "place_amenity", Base.metadata,
         Column("place_id", String(60), ForeignKey(
-            "places.id"), primary_key=True, nullable=False),
+            "places.id"), primary_key=True, nullable=False,
+            cascade="all, delete"),
         Column("amenity_id", String(60), ForeignKey(
-            "amenities.id"), primary_key=True, nullable=False))
+            "amenities.id"), primary_key=True, nullable=False,
+            cascade="all, delete"))
 
 
 class Place(BaseModel, Base):
