76b153ad529aa751257fb33d4f81a6ca2bc94101
aarizat <aarizatr@gmail.com>
2020-12-17 16:32:19 +0000

    Change secondary paramater

--- models/amenity.py
+++ models/amenity.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
+from models.place import Place
 
 
 class Amenity(BaseModel, Base):
@@ -13,8 +14,7 @@ class Amenity(BaseModel, Base):
         name = Column(String(128), nullable=False)
         place_amenities = relationship(
             "Place",
-            secondary="place_amenity",
-            back_populates="amenities",
+            secondary=Place.place_amenity,
             viewonly=False)
     else:
         name = ""
