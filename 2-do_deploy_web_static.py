042683f3ca1f1f84d2fdfae6b9ed92fa735b4ec5
aarizat <aarizatr@gmail.com>
2020-12-23 01:29:58 +0000

    Delete fucntion and change if statement

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,25 +2,11 @@
 """
 Deploy web page to server
 """
-from fabric.api import local, put, run, env
-from datetime import datetime
+from fabric.api import put, run, env
 from os import path
 
 
 env.hosts = ["35.229.34.27", "35.237.166.174"]
-
-
-def do_pack():
-    """
-    Compress a folder to .tgz archive.
-    """
-    date = datetime.utcnow()
-    path = "versions/web_static_{}.tgz".format(
-        datetime.strftime(date, "%Y%m%d%H%M%S"))
-    local("mkdir -p versions")
-    if local("tar -cvzf {} web_static".format(path)).failed:
-        return
-    return path
 
 
 def do_deploy(archive_path):
@@ -30,16 +16,16 @@ def do_deploy(archive_path):
     Args:
         archive_path (str): has the form "versions/web_static_YMDHMS.tgz"
     """
-    if not path.exists(archive_path):
-        return False
-    file_tar = archive_path.split("/")[-1]  # web_static_YMDHMS.tgz
-    server_path = "/data/web_static/releases/{}".format(file_tar[:-4])
-    run("mkdir -p {}".format(server_path))  # create path in the remote server
-    put(archive_path, "/tmp/")  # copy from local to remote
-    run("tar -xzf /tmp/{} -C {}".format(file_tar, server_path))
-    run("rm /tmp/{}".format(file_tar))
-    run("mv -f {}/web_static/* {}/".format(server_path, server_path))
-    run('rm -rf {}/web_static'.format(server_path))
-    run("rm -rf /data/web_static/current")
-    run("ln -sf {} /data/web_static/current".format(server_path))
-    return True
+    if path.exists(archive_path):
+        file_tar = archive_path.split("/")[-1]  # web_static_YMDHMS.tgz
+        server_path = "/data/web_static/releases/{}".format(file_tar[:-4])
+        run("mkdir -p {}".format(server_path))  # create path in the server
+        put(archive_path, "/tmp/")  # copy from local to remote
+        run("tar -xzf /tmp/{} -C {}".format(file_tar, server_path))
+        run("rm /tmp/{}".format(file_tar))
+        run("mv -f {}/web_static/* {}/".format(server_path, server_path))
+        run('rm -rf {}/web_static'.format(server_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -sf {} /data/web_static/current".format(server_path))
+        return True
+    return False
